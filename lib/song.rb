fbdfa5c770b639c72a92f0f2da85da2aa3b66ca1
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-10-15 13:51:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,8 +22,8 @@ class Song
   end
   
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    # artist.add_song(self)
+    #self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
     
 end
